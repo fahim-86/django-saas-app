9d4ac46221c2869a1f1f6384b42543627e0bd698
fahim-86 <fam.fahim@yahoo.com>
2024-06-22 02:51:25 +1000

    Add data table on model

--- src/usrvisit/models.py
+++ src/usrvisit/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+
+class PageVisit(models.Model):
+    urlPath = models.TextField(blank=True, null=True)
+    timestamp = models.DateTimeField(auto_now_add=True)
