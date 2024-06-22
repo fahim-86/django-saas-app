9136cfae039fa15eb9d4b6f477fe8e9d2444f02f
fahim-86 <fam.fahim@yahoo.com>
2024-06-22 17:36:31 +1000

    Work with the DB Model file

--- src/usrvisit/models.py
+++ src/usrvisit/models.py
@@ -3,5 +3,5 @@ from django.db import models
 # Create your models here.
 
 class PageVisit(models.Model):
-    urlPath = models.TextField(blank=True, null=True)
+    path = models.TextField(blank=True, null=True)
     timestamp = models.DateTimeField(auto_now_add=True)
